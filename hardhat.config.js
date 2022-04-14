f68074d1748d20e7c181f3dcf1a6a043603a456c
rohit-710 <rrohitramesh710@gmail.com>
2022-04-14 23:16:12 +0530

    Final version

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectID = "41c09f593cdf478fa7208e63e018b2c5"
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainID: 1337
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.infura.io/v3/41c09f593cdf478fa7208e63e018b2c5",
+      accounts: [privateKey]
+    },
+    mainet: {
+      url: "https://polygon-mainnet.infura.io/v3/${projectID}",
+      accounts: [privateKey]
+    }
+  },
+  localhost: {
+    url: "http://localhost:8545",
+    chainId: 31337
+  },
+  solidity: "0.8.4",
+};
